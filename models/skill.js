77c97a76e5ece2d307c23a45d343692c27b72adb
aaronfnp <aaronfnp@gmail.com>
2024-02-14 15:40:42 -0500

    part 1 completed

--- models/skill.js
+++ models/skill.js
@@ -1,1 +1,21 @@
-c
+const skills = [
+  { id: 1, skill: "Cooking", mastery: "Amateur" },
+  { id: 2, skill: "Gaming", mastery: "Moderate" },
+  { id: 3, skill: "Coding", mastery: "Amateur" },
+  { id: 4, skill: "Sleeping", mastery: "Master" },
+  { id: 4, skill: "Dancing", mastery: "Extremely Poor" },
+];
+
+module.exports = {
+  getAll,
+  getOne,
+};
+
+function getAll() {
+  return skills;
+}
+
+function getOne(id) {
+  const idNum = parseInt(id);
+  return skills.find((skill) => skill.id === idNum);
+}
